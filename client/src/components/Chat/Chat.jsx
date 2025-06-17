44689c6519aa864a6114a44c5c86a5b549fb1b74
Benfranklin m s <bfranklinms@gmail.com>
2025-06-17 14:57:35 +0530

    feat: integrate socket.io for real-time chat functionality
    - Added socket.io-client to the client dependencies for WebSocket communication.
    - Implemented chat room joining logic in Chat component using query-string for URL parsing.
    - Configured CORS in the server to allow requests from the client.
    - Set up socket connection and event handling for user joining and disconnecting in the server.

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import { useLocation } from 'react-router-dom';
+import { io } from "socket.io-client";
+
+
+let socket;
 
 const Chat = () => {
+  const [name, setname] = useState("");
+  const [room, setroom] = useState("");
+  const ENDPOINT = "http://localhost:3000";
+  const location = useLocation();
+  useEffect(() => {
+    const { name, room }= queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setname(name);
+    setroom(room);
+    socket.emit('join', { name, room }, () => {
+
+    });
+
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    }
+  }, [ENDPOINT, location.search]);
+  
   return (
    <h1>Chat</h1>
   )
--- server/server.js
+++ server/server.js
@@ -2,18 +2,36 @@ import express from 'express';
 import { Server } from "socket.io";
 import http from 'http';
 import { createServer } from 'http';
+import cors from 'cors';
 import router from './routes/router.js';
+import { error } from 'console';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
 
+app.use(cors({
+    origin: 'http://localhost:5173', // Adjust this to your client URL
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+
 io.on('connection', (socket) => {
     console.log("connected");
+
+    socket.on('join', ({ name, room }) => {
+        console.log(`${name} has joined the room: ${room}`);
+    })
 
     socket.on("disconnect", () => {
         console.log("disconnected");
