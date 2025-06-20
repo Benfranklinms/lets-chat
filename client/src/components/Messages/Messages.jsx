dac0dc34a386aec71ff551896c8247c2c8a5e90d
Benfranklin m s <bfranklinms@gmail.com>
2025-06-20 21:28:24 +0530

    feat: implement Message component and update Messages to render individual messages

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ScrollToBottom from 'react-scroll-to-bottom'
+import Message from '../Message/Message';
 
 const Messages = ({ messages, name }) => {
   return (
     <div>
         <ScrollToBottom>
             {messages.map((messages, index) => {
-                <div key = {index}> Message message = {messages} name = {name} </div>
+                <div key = {index}> <Message message = {messages} name = {name} /> </div>
             })}
         </ScrollToBottom>
     </div>
