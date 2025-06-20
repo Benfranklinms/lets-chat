5908145a365afcaa4a9fed3a10cc083221138f11
Benfranklin m s <bfranklinms@gmail.com>
2025-06-20 23:02:50 +0530

    some changes

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -8,16 +8,16 @@ const Join = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
       <div className='bg-white p-12 rounded shadow-md w-80'>
-        <h1 className='text-xl mb-4 text-bold'>Join a room</h1>
+        <h1 className='text-xl mb-4 font-bold'>Join a room</h1>
         <div className='mb-4 ring ring-gray-300 rounded'>
-          <input type="text" placeholder="Name" className='p-2 outline-none w-full' onChange={(event) => setname(event.target.value)}/>
+          <input type="text" placeholder="Name" className='p-2 outline-none w-full ring ring-lime-200' onChange={(event) => setname(event.target.value)}/>
         </div>
         <div className='mb-4 ring ring-gray-300 rounded'>
-          <input type="text" placeholder="Room" className='p-2 outline-none w-full' onChange={(event) => setroom(event.target.value)}/>
+          <input type="text" placeholder="Room" className='p-2 outline-none w-full ring ring-lime-200' onChange={(event) => setroom(event.target.value)}/>
         </div>
         <div className='flex justify-start'>
           <Link onClick = {event => (!name || !room)? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button className='bg-indigo-500 hover:bg-indigo-400 text-white px-6 py-2 rounded' type='submit'>
+          <button className='bg-lime-200 hover:bg-lime-500 text-black px-6 py-2 rounded font-bold cursor-pointer' type='submit'>
             Join
           </button>
           </Link>
