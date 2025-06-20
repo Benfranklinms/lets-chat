c582a956fab2c7c840b8e8929967158935de47fd
Benfranklin m s <bfranklinms@gmail.com>
2025-06-20 11:34:55 +0530

    feat: add Infobar component with online and close icons

--- client/src/components/Infobar/Infobar.jsx
+++ client/src/components/Infobar/Infobar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import onlineIcon from '../../icons/onlineIcon.png';
+import closeIcon from '../../icons/closeIcon.png';
 
 const Infobar = () => {
   return (
     <div>
-        
+        <div>
+            <img src = {onlineIcon} alt="User" />
+            <h3>Room Name</h3>
+        </div>
+            <a href="/">
+                <img src = {closeIcon} alt=""/>
+            </a>
+        <div>
+
+        </div>
     </div>
   )
 }
